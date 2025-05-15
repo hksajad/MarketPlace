6475459c45b3be163e256d1c230a51b077940204
sajjad <sajjadhasanikakh06@gmail.com>
2025-05-15 22:05:03 +0330

    added top header

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('ul');
+const toggle = document.querySelector(".toggle_menu-open");
+toggle.addEventListener('click' , function (){ 
+    menu.classList.toggle("show_menu")
+})
