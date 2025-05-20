05d0e04aef29f3669d6e1a576c06a1919d6e3ba1
ehsan <ehsantahamii@gmail.com>
2025-05-20 20:16:45 +0330

    commit #add products section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // start js navbar
 const menu = document.querySelector("ul");
-const menu = document.querySelector("ul");
 const toggle = document.querySelector(".toggle_menu-open");
 toggle.addEventListener("click", function () {
   menu.classList.toggle("show_menu");
